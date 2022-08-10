c075612b7a301b4429b26ae2eacc34cfc0658f7d
KseniiaOs <Ksu229@list.ru>
2022-08-10 17:44:42 +0300

    update src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+        if (n > 0) {
+        const s = n.toString();
+        const newS = s.split('');
+        const revS = newS.reverse();
+        const Num = revS.join('');
+        return Num;
+        } else {
+        const t = -n;    
+        const s = t.toString();
+        const newS = s.split('');
+        const revS = newS.reverse();
+        const Num = revS.join('');
+        return Num;    
+        }
 }
